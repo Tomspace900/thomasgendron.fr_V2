75e55e47206fbfe5ede94991191d143604a9e2c5
Thomas <tomspace.800@gmail.com>
2024-02-27 22:11:02 +0100

    [feat] init shadnc library & tabler icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Toaster } from '@/components/ui/toaster';
 
 const inter = Inter({ subsets: ["latin"] });
+
+import { cn } from '@/lib/utils';
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +18,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+		<html lang='en'>
+			<body className={cn(inter.className, 'bg-background')}>{children}</body>
+			<Toaster />
     </html>
   );
 }
