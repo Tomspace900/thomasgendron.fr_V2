4e69f3d012671a50d0623a0d72e6e505885df9e2
Thomas <tomspace.800@gmail.com>
2024-02-28 16:32:45 +0100

    [feat] grainy background

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,9 @@ const config = {
       },
     },
     extend: {
+			fontFamily: {
+				sans: ["var(--font-main)"],
+			},
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
